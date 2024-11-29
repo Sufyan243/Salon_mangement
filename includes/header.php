cb74095a8bb28fec8f61628e196ff6ac458622f6
Sufyan <Syedmuhammadsufyan237@gmail.com>
2024-11-30 04:12:02 +0500

    Ahmed tasks

--- includes/header.php
+++ includes/header.php
@@ -23,7 +23,10 @@
                                     <li><a class="menu-item" href="services.php">Services</a>
                                         <ul>
                                             <li><a class="menu-item" href="services.php">All Services</a></li>
-                                            <li><a class="menu-item" href="service-single.php">Service Single</a></li>
+                                            <li><a class="menu-item" href="haircut.php">Haircut</a></li>
+                                            <li><a class="menu-item" href="shave.php">Shave</a></li>
+                                            <li><a class="menu-item" href="facial.php">Facial</a></li>
+                                            <li><a class="menu-item" href="package.php">Package</a></li>
                                         </ul>
                                     </li>
                                     <li><a class="menu-item" href="about.html">About</a>
@@ -46,7 +49,65 @@
                             </div>
                             <div class="de-flex-col">
                                 <div class="menu_side_area">
-                                    <a href="book.php" class="btn-main">Book Now</a>
+
+          <!-- Book Now and Login Buttons with Dropdown -->
+<div class="d-inline-block wow fadeInRight" data-wow-delay=".5s">
+  <a href="book.php" class="btn-main">Book Now</a>
+</div>
+
+<div class="btn-group wow fadeInRight" data-wow-delay=".6s" style="margin-left: 15px;">
+  <button type="button" class="btn-main dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+    Login
+  </button>
+  <ul class="dropdown-menu" style="background-color: #f5f5dc; border: 1px solid #d2b48c;">
+    <li><a class="dropdown-item" href="admin-login.php" style="color: #ffffff; font-weight: bold;">Admin</a></li>
+    <li><a class="dropdown-item" href="user-login.php" style="color: #ffffff; font-weight: bold;">User</a></li>
+  </ul>
+</div>
+
+<!-- Inline CSS for Styling -->
+<style>
+  .btn-main {
+    padding: 12px 30px; /* Ensures buttons have the same padding */
+    background-color: #d58963; /* Matching color */
+    color: white;
+    border: none;
+    border-radius: 5px;
+    text-transform: uppercase;
+    font-weight: bold;
+    text-align: center;
+    display: inline-block;
+    line-height: 1.5; /* Ensures consistent height */
+  }
+
+  .btn-main:hover {
+    background-color: #b76e49; /* Hover effect */
+  }
+
+  .dropdown-toggle::after {
+    display: none; /* Removes white caret line */
+  }
+
+  .dropdown-menu {
+    background-color: #d58963; /* Dropdown matching color */
+    border: none;
+    border-radius: 5px;
+  }
+
+  .dropdown-item {
+    color: #ffffff; /* White text in dropdown */
+  }
+
+  .dropdown-item:hover {
+    background-color: #b76e49; /* Hover color match */
+    color: #ffffff;
+  }
+</style>
+
+
+
+
+
                                     <span id="menu-btn"></span>
                                 </div>
                             </div>
---
+++ package.php
@@ -0,0 +1,226 @@
+<!DOCTYPE html>
+<html lang="zxx">
+
+
+<!-- Mirrored from madebydesignesia.com/themes/blaxcut/service-single.html by HTTrack Website Copier/3.x [XR&CO'2014], Sat, 23 Nov 2024 10:43:35 GMT -->
+<head>
+    <title>Blaxcut - Barbershop Website Template</title>
+    <link rel="icon" href="images/icon.png" type="image/gif" sizes="16x16">
+    <meta content="text/html;charset=utf-8" http-equiv="Content-Type">
+    <meta content="width=device-width, initial-scale=1.0" name="viewport" >
+    <meta content="Blaxcut - Barbershop Website Template" name="description" >
+    <meta content="" name="keywords" >
+    <meta content="" name="author" >
+    <!-- CSS Files
+    ================================================== -->
+    <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css" id="bootstrap">
+    <link href="css/mdb.min.css" rel="stylesheet" type="text/css" id="mdb" >
+    <link href="css/plugins.css" rel="stylesheet" type="text/css" >
+    <link href="css/style.css" rel="stylesheet" type="text/css" >
+    <link href="css/coloring.css" rel="stylesheet" type="text/css" >
+    <!-- color scheme -->
+    <link id="colors" href="css/colors/scheme-01.css" rel="stylesheet" type="text/css" >
+</head>
+
+<body class="dark-scheme">
+    <div id="wrapper">
+        
+        <!-- page preloader begin -->
+        <div id="de-loader"></div>
+        <!-- page preloader close -->
+
+        <!-- header begin -->
+        <?php  include('includes/header.php') ?>
+
+        <!-- header close -->
+        <!-- content begin -->
+        <div class="no-bottom no-top" id="content">
+            <div id="top"></div>
+
+            <section id="subheader" class="jarallax">
+                <img src="images/background/2.jpg" class="jarallax-img" alt="">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-lg-6 offset-lg-3 text-center">
+                            <h1>PACKAGE</h1>
+                            <div class="de-separator"></div>
+                        </div>
+                    </div>
+                </div>
+                <div class="de-gradient-edge-bottom"></div>
+            </section>
+
+            <section aria-label="section" class="no-top no-bottom">
+
+                <div class="container">
+                    <div class="row align-items-center">
+                        <div class="col-lg-6 offset-lg-3 text-center">
+                            <img src="images/misc/man-3-b.png" class="img-fluid wow fadeInUp" alt="">
+                        </div>
+                        <div class="col-lg-8 offset-lg-2 text-center" data-jarallax-element="-20">
+                            <h2 class="wow fadeInUp">Blaxcut Crafting <span class="id-color">Confidence</span>, One Cut at a Time</h2>
+                            <p class="lead wow fadeInUp">When it comes to personal grooming and style, a haircut can make all the difference. It's not just about trimming those unruly locks; it's an art form that can transform your entire look and boost your confidence. At our establishment, we take immense pride in offering a comprehensive range of haircut services that cater to your unique preferences and individuality.
+                            </p>
+                            <p class="lead wow fadeInUp">Our commitment to excellence extends beyond the scissors and clippers. We believe in creating an environment where you can relax and unwind, a place where you can escape the hustle and bustle of everyday life. That's why we offer more than just haircuts; we provide an opportunity to rejuvenate, to chat, and to walk out feeling like the best version of yourself.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+            <section aria-label="section" class="no-bottom">
+                <div class="wow fadeInRight d-flex">
+                  <div class="de-marquee-list wow">
+                    <div class="d-item">
+                      <span class="d-item-txt">Slickback</span>
+                      <span class="d-item-display">
+                        <i class="d-item-block"></i>
+                      </span>
+                      <span class="d-item-txt">Classic Quiff</span>
+                      <span class="d-item-display">
+                        <i class="d-item-block"></i>
+                      </span>
+                      <span class="d-item-txt">Pompadour</span>
+                      <span class="d-item-display">
+                        <i class="d-item-block"></i>
+                      </span>
+                      <span class="d-item-txt">Fringe</span>
+                      <span class="d-item-display">
+                        <i class="d-item-block"></i>
+                      </span>
+                      <span class="d-item-txt">Beard Trim</span>
+                      <span class="d-item-display">
+                        <i class="d-item-block"></i>
+                      </span>
+                      <span class="d-item-txt">Curly Fade</span>
+                      <span class="d-item-display">
+                        <i class="d-item-block"></i>
+                      </span>
+                      <span class="d-item-txt">Undercut</span>
+                      <span class="d-item-display">
+                        <i class="d-item-block"></i>
+                      </span>
+                     </div>
+                  </div>
+                </div>
+            </section>
+
+            <section class="jarallax">
+                <div class="de-gradient-edge-top"></div>
+                <div class="de-gradient-edge-bottom"></div>
+                <img src="images/background/1.jpg" class="jarallax-img" alt="">
+                <div class="container position-relative z1000">
+                    <div class="row">
+                        <div class="col-lg-8 offset-lg-2 text-center">
+                            <h2 class="wow fadeInUp">Hair <span class="id-color">Styles</span></h2>
+                            <div class="spacer-single"></div>
+                        </div>
+                    </div>
+                    <div id="gallery" class="row g-4 wow fadeInUp">
+
+                        <div class="col-md-4 item">
+                            <div class="de-image-hover rounded">
+                                <a href="images/hairstyles/1.jpg" class="image-popup">                                
+                                    <span class="dih-title-wrap">
+                                        <span class="dih-title">Slickback</span>
+                                    </span>
+                                    <span class="dih-overlay"></span>
+                                    <img src="images/hairstyles/1.jpg" class="lazy img-fluid" alt="">
+                                </a>
+                            </div>
+                        </div>
+                        
+                        <div class="col-md-4 item">
+                            <div class="de-image-hover rounded">
+                                <a href="images/hairstyles/2.jpg" class="image-popup">                                
+                                    <span class="dih-title-wrap">
+                                        <span class="dih-title">Classic Quiff</span>
+                                    </span>
+                                    <span class="dih-overlay"></span>
+                                    <img src="images/hairstyles/2.jpg" class="lazy img-fluid" alt="">
+                                </a>
+                            </div>
+                        </div>
+                        
+                        <div class="col-md-4 item">
+                            <div class="de-image-hover rounded">
+                                <a href="images/hairstyles/3.jpg" class="image-popup">                                
+                                    <span class="dih-title-wrap">
+                                        <span class="dih-title">Pompadour</span>
+                                    </span>
+                                    <span class="dih-overlay"></span>
+                                    <img src="images/hairstyles/3.jpg" class="lazy img-fluid" alt="">
+                                </a>
+                            </div>
+                        </div>
+                        
+                        <div class="col-md-4 item">
+                            <div class="de-image-hover rounded">
+                                <a href="images/hairstyles/4.jpg" class="image-popup">                                
+                                    <span class="dih-title-wrap">
+                                        <span class="dih-title">Fringe</span>
+                                    </span>
+                                    <span class="dih-overlay"></span>
+                                    <img src="images/hairstyles/4.jpg" class="lazy img-fluid" alt="">
+                                </a>
+                            </div>
+                        </div>
+
+                        <div class="col-md-4 item">
+                            <div class="de-image-hover rounded">
+                                <a href="images/hairstyles/5.jpg" class="image-popup">                                
+                                    <span class="dih-title-wrap">
+                                        <span class="dih-title">Curly Fade</span>
+                                    </span>
+                                    <span class="dih-overlay"></span>
+                                    <img src="images/hairstyles/5.jpg" class="lazy img-fluid" alt="">
+                                </a>
+                            </div>
+                        </div>
+                        
+                        <div class="col-md-4 item">
+                            <div class="de-image-hover rounded">
+                                <a href="images/hairstyles/6.jpg" class="image-popup">                                
+                                    <span class="dih-title-wrap">
+                                        <span class="dih-title">Undercut</span>
+                                    </span>
+                                    <span class="dih-overlay"></span>
+                                    <img src="images/hairstyles/6.jpg" class="lazy img-fluid" alt="">
+                                </a>
+                            </div>
+                        </div>
+
+                        <div class="col-md-4 item">
+                            <div class="de-image-hover rounded">
+                                <a href="images/hairstyles/7.php" class="image-popup">                             
+                                    <span class="dih-title-wrap">
+                                        <span class="dih-title">Blaxcut Barbershop</span>
+                                    </span>
+                                    <span class="dih-overlay"></span>
+                                    <img src="images/hairstyles/7.php" class="lazy img-fluid" alt="">
+                                </a>
+                            </div>
+                        </div>                        
+                    </div>
+                </div>
+            </section>
+            
+        </div>
+        <!-- content close -->
+        <a href="#" id="back-to-top"></a>
+        <!-- footer begin -->
+        <?php  include('includes/footer.php') ?>
+
+        <!-- footer close -->
+    </div>
+    
+    <!-- Javascript Files
+    ================================================== -->
+    <script src="js/plugins.js"></script>
+    <script src="js/designesia.js"></script>
+    <script src="js/custom-marquee.js"></script>
+</body>
+
+
+<!-- Mirrored from madebydesignesia.com/themes/blaxcut/service-single.html by HTTrack Website Copier/3.x [XR&CO'2014], Sat, 23 Nov 2024 10:43:36 GMT -->
+</html>
