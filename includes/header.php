2aaf162e193c1a7f4c1aa19e085d714edfe455c9
exam <syedmuhammadsufyan237@gmail.com>
2024-11-30 17:20:44 +0500

    update

--- includes/header.php
+++ includes/header.php
@@ -1,118 +1,72 @@
-<header class="transparent">            
-            <div class="container">
-                <div class="row">
-                    <div class="col-md-12">
-                        <div class="de-flex sm-pt10">
-                            <div class="de-flex-col">
-                                <div class="de-flex-col">
-                                    <!-- logo begin -->
-                                    <div id="logo">
-                                        <a href="index.html">
-                                            <img class="logo-main" src="images/logo.png" alt="" >
-                                            <img class="logo-mobile" src="images/logo-mobile.png" alt="" >
-                                        </a>
-                                    </div>
-                                    <!-- logo close -->
-                                </div>
-                            </div>
-                            <div class="de-flex-col header-col-mid">
-                                <ul id="mainmenu">
-                                    <li><a class="menu-item" href="index.php">Home</a>
-                                       
-                                    </li>
-                                    <li><a class="menu-item" href="services.php">Services</a>
-                                        <ul>
-                                            <li><a class="menu-item" href="services.php">All Services</a></li>
-                                            <li><a class="menu-item" href="haircut.php">Haircut</a></li>
-                                            <li><a class="menu-item" href="shave.php">Shave</a></li>
-                                            <li><a class="menu-item" href="facial.php">Facial</a></li>
-                                            <li><a class="menu-item" href="package.php">Package</a></li>
-                                        </ul>
-                                    </li>
-                                    <li><a class="menu-item" href="about.html">About</a>
-                                        <ul>
-                                            <li><a class="menu-item" href="about.php">About Us</a></li>
-                                            <li><a class="menu-item" href="team.php">Our Team</a></li>
-                                        </ul>
-                                    </li>
-                                    <li><a class="menu-item" href="book.php">Book Appointment</a></li>
-                                    <li><a class="menu-item" href="blog.php">Blog</a></li>
-                                    <li><a class="menu-item" href="#">Extras</a>
-                                        <ul>
-                                            <li><a class="menu-item" href="contact.php">Contact</a></li>
-                                            <li><a class="menu-item" href="gallery.php">Gallery</a></li>
-                                            <li><a class="menu-item" href="pricing.php">Pricing</a></li>
-                                            <li><a class="menu-item" href="testimonials.php">Testimonials</a></li>
-                                        </ul>
-                                    </li>
-                                </ul>
-                            </div>
-                            <div class="de-flex-col">
-                                <div class="menu_side_area">
-
-          <!-- Book Now and Login Buttons with Dropdown -->
-<div class="d-inline-block wow fadeInRight" data-wow-delay=".5s">
-  <a href="book.php" class="btn-main">Book Now</a>
-</div>
-
-<div class="btn-group wow fadeInRight" data-wow-delay=".6s" style="margin-left: 15px;">
-  <button type="button" class="btn-main dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
-    Login
-  </button>
-  <ul class="dropdown-menu" style="background-color: #f5f5dc; border: 1px solid #d2b48c;">
-    <li><a class="dropdown-item" href="admin-login.php" style="color: #ffffff; font-weight: bold;">Admin</a></li>
-    <li><a class="dropdown-item" href="user-login.php" style="color: #ffffff; font-weight: bold;">User</a></li>
-  </ul>
-</div>
-
-<!-- Inline CSS for Styling -->
-<style>
-  .btn-main {
-    padding: 12px 30px; /* Ensures buttons have the same padding */
-    background-color: #d58963; /* Matching color */
-    color: white;
-    border: none;
-    border-radius: 5px;
-    text-transform: uppercase;
-    font-weight: bold;
-    text-align: center;
-    display: inline-block;
-    line-height: 1.5; /* Ensures consistent height */
-  }
-
-  .btn-main:hover {
-    background-color: #b76e49; /* Hover effect */
-  }
-
-  .dropdown-toggle::after {
-    display: none; /* Removes white caret line */
-  }
-
-  .dropdown-menu {
-    background-color: #d58963; /* Dropdown matching color */
-    border: none;
-    border-radius: 5px;
-  }
-
-  .dropdown-item {
-    color: #ffffff; /* White text in dropdown */
-  }
-
-  .dropdown-item:hover {
-    background-color: #b76e49; /* Hover color match */
-    color: #ffffff;
-  }
-</style>
-
-
-
-
-
-                                    <span id="menu-btn"></span>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+<header class="transparent">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12">
+        <div class="de-flex sm-pt10">
+          <div class="de-flex-col">
+            <div class="de-flex-col">
+              <!-- logo begin -->
+              <div id="logo">
+                <a href="index.html">
+                  <img class="logo-main" src="images/logo.png" alt="">
+                  <img class="logo-mobile" src="images/logo-mobile.png" alt="">
+                </a>
+              </div>
+              <!-- logo close -->
             </div>
-        </header>
+          </div>
+          <div class="de-flex-col header-col-mid">
+            <ul id="mainmenu">
+              <li><a class="menu-item" href="index.php">Home</a>
+
+              </li>
+              <li><a class="menu-item" href="services.php">Services</a>
+                <ul>
+                  <li><a class="menu-item" href="services.php">All Services</a></li>
+                  <li><a class="menu-item" href="haircut.php">Haircut</a></li>
+                  <li><a class="menu-item" href="shave.php">Shave</a></li>
+                  <li><a class="menu-item" href="facial.php">Facial</a></li>
+                  <li><a class="menu-item" href="package.php">Package</a></li>
+                </ul>
+              </li>
+              <li><a class="menu-item" href="about.html">About</a>
+                <ul>
+                  <li><a class="menu-item" href="about.php">About Us</a></li>
+                  <li><a class="menu-item" href="team.php">Our Team</a></li>
+                </ul>
+              </li>
+              <li><a class="menu-item" href="book.php">Book Appointment</a></li>
+              <li><a class="menu-item" href="blog.php">Blog</a></li>
+              <li><a class="menu-item" href="#">Extras</a>
+                <ul>
+                  <li><a class="menu-item" href="contact.php">Contact</a></li>
+                  <li><a class="menu-item" href="gallery.php">Gallery</a></li>
+                  <li><a class="menu-item" href="pricing.php">Pricing</a></li>
+                  <li><a class="menu-item" href="testimonials.php">Testimonials</a></li>
+                </ul>
+              </li>
+            </ul>
+          </div>
+          <div class="de-flex-col">
+            <div class="menu_side_area">
+
+              <!-- Book Now and Login Buttons with Dropdown -->
+              <div class="d-inline-block wow fadeInRight" data-wow-delay=".5s">
+                <a href="" class="btn-main " data-bs-toggle="dropdown" aria-expanded="false">Login</a>
+                <ul class="dropdown-menu">
+                  <li><a class="dropdown-item" href="admin-login.php">Admin</a></li>
+                  <li><a class="dropdown-item" href="user-login.php">User</a></li>
+                </ul>
+              </div>
+              <div class="d-inline-block wow fadeInRight" data-wow-delay=".5s">
+                <a href="book.php" class="btn-main">Book Now</a>
+              </div>
+
+              <span id="menu-btn"></span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</header>
